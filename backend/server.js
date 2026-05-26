eb868f81b165eb443e46b83c4ea3947fc6891446
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2026-05-26 18:34:33 +0530

    remove dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const path = require("path");   // ADD THIS
 
-dotenv.config();
+// dotenv.config();
 connectDB();
 
 const app = express();
