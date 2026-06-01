661fd275aae115a0085cdf9a6743bf5b8832c50e
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2026-06-01 11:57:52 +0530

    Fixed MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Server Connected Successfully");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Server Connection Failed:", error.message);
+    console.error("MongoDB Connection Error:", error);
     process.exit(1);
   }
 };
