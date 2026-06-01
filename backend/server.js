5b1dc7c53baca780b4690849b6465ebfa8b6c3a1
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2026-06-01 12:19:35 +0530

    Fixed undefined port

--- backend/server.js
+++ backend/server.js
@@ -66,8 +66,8 @@ app.use(express.urlencoded({
 
 /* =============================== */
 
-// const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT;
 
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
