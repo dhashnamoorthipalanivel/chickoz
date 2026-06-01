45e7df1d6e9592abaa7090096f0a50288190d27a
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2026-06-01 11:51:05 +0530

    Fixed host binding

--- backend/server.js
+++ backend/server.js
@@ -74,9 +74,14 @@ app.get("*", (req, res) => {
 
 /* =============================== */
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
