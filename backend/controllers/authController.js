cb0e4f27cb40bce0adf56f459d57e447aa7732ac
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2026-06-01 12:25:30 +0530

    Test franchise import

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 const User = require("../models/User");
 const generateToken = require("../utils/generateToken");
-const Franchise = require("../models/masterModels/franchiseModel");
+// const Franchise = require("../models/masterModels/franchiseModel");
 
 // @desc    Register user
 // @route   POST /api/auth/register
@@ -226,9 +226,9 @@ const getMe = async (req, res) => {
     let franchise = null;
 
     // ✅ FRANCHISE USER
-    if (user.franchiseId) {
-      franchise = await Franchise.findById(user.franchiseId);
-    }
+    // if (user.franchiseId) {
+    //   franchise = await Franchise.findById(user.franchiseId);
+    // }
 
     res.status(200).json({
       user,
