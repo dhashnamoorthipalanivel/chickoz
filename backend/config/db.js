6e7d26326f2197ea751845e8c64f09cde540ca5d
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2026-06-01 12:09:35 +0530

    Fixed mongodb timeout

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
