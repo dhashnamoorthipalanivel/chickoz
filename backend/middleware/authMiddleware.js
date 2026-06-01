c90389441d71613f4459831c02732a6785967ab2
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2026-06-01 13:04:09 +0530

    Minimal middleware test

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,88 +1,94 @@
+// // const jwt = require("jsonwebtoken");
+// // const User = require("../models/User");
+
+// // const protect = async (req, res, next) => {
+// //   let token;
+
+// //   if (
+// //     req.headers.authorization &&
+// //     req.headers.authorization.startsWith("Bearer")
+// //   ) {
+// //     try {
+// //       token = req.headers.authorization.split(" ")[1];
+
+// //       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+// //       req.user = await User.findById(decoded.id).select("-password");
+
+// //       if (!req.user) {
+// //         return res.status(401).json({
+// //           success: false,
+// //           message: "User not found",
+// //         });
+// //       }
+
+// //       next();
+// //     } catch (error) {
+// //       return res.status(401).json({
+// //         success: false,
+// //         message: "Not authorized, token failed",
+// //       });
+// //     }
+// //   }
+
+// //   if (!token) {
+// //     return res.status(401).json({
+// //       success: false,
+// //       message: "Not authorized, no token",
+// //     });
+// //   }
+// // };
+
+// // module.exports = { protect };
+
+
 // const jwt = require("jsonwebtoken");
 // const User = require("../models/User");
 
 // const protect = async (req, res, next) => {
-//   let token;
+//   try {
+//     let token;
 
-//   if (
-//     req.headers.authorization &&
-//     req.headers.authorization.startsWith("Bearer")
-//   ) {
-//     try {
+//     if (
+//       req.headers.authorization &&
+//       req.headers.authorization.startsWith("Bearer")
+//     ) {
 //       token = req.headers.authorization.split(" ")[1];
 
 //       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-//       req.user = await User.findById(decoded.id).select("-password");
+//       const user = await User.findById(decoded.id).select("-password");
 
-//       if (!req.user) {
+//       if (!user) {
 //         return res.status(401).json({
 //           success: false,
 //           message: "User not found",
 //         });
 //       }
 
+//       req.user = user;
+
 //       next();
-//     } catch (error) {
+//     } else {
 //       return res.status(401).json({
 //         success: false,
-//         message: "Not authorized, token failed",
+//         message: "No token provided",
 //       });
 //     }
-//   }
+//   } catch (error) {
+//     console.error("Auth Middleware Error:", error);
 
-//   if (!token) {
 //     return res.status(401).json({
 //       success: false,
-//       message: "Not authorized, no token",
+//       message: "Token failed",
 //     });
 //   }
 // };
 
 // module.exports = { protect };
 
-
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
-
 const protect = async (req, res, next) => {
-  try {
-    let token;
-
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      token = req.headers.authorization.split(" ")[1];
-
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      const user = await User.findById(decoded.id).select("-password");
-
-      if (!user) {
-        return res.status(401).json({
-          success: false,
-          message: "User not found",
-        });
-      }
-
-      req.user = user;
-
-      next();
-    } else {
-      return res.status(401).json({
-        success: false,
-        message: "No token provided",
-      });
-    }
-  } catch (error) {
-    console.error("Auth Middleware Error:", error);
-
-    return res.status(401).json({
-      success: false,
-      message: "Token failed",
-    });
-  }
+  next();
 };
 
 module.exports = { protect };
