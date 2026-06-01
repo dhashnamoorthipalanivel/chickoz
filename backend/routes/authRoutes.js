d38ea74196b0fde2b28b9d9929b8928401bea489
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2026-06-01 13:50:27 +0530

    minimal auth route test

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,29 +1,43 @@
-const express = require("express");
-const {
-  registerUser,
-  loginUser,
-  forgotPassword,
-  resetPassword,
-  getMe,
-  logoutUser,
-  sendChangePasswordOtp,
-  verifyChangePasswordOtp,
-} = require("../controllers/authController");
+// const express = require("express");
+// const {
+//   registerUser,
+//   loginUser,
+//   forgotPassword,
+//   resetPassword,
+//   getMe,
+//   logoutUser,
+//   sendChangePasswordOtp,
+//   verifyChangePasswordOtp,
+// } = require("../controllers/authController");
 
-const { protect } = require("../middleware/authMiddleware");
+// const { protect } = require("../middleware/authMiddleware");
+// const router = express.Router();
+
+// router.post("/register", registerUser);
+// router.post("/login", loginUser);
+// router.post("/forgot-password", forgotPassword);
+// router.post("/reset-password/:token", resetPassword);
+// router.get("/me", protect, getMe);
+// router.post("/logout", protect, logoutUser);
+// router.post("/send-change-password-otp", protect, sendChangePasswordOtp);
+// router.post("/verify-change-password-otp", protect, verifyChangePasswordOtp);
+
+// router.get("/", (req, res) => {
+//   res.send("Auth Route Working");
+// });
+
+// module.exports = router;
+
+
+const express = require("express");
+
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.post("/forgot-password", forgotPassword);
-router.post("/reset-password/:token", resetPassword);
-router.get("/me", protect, getMe);
-router.post("/logout", protect, logoutUser);
-router.post("/send-change-password-otp", protect, sendChangePasswordOtp);
-router.post("/verify-change-password-otp", protect, verifyChangePasswordOtp);
-
-router.get("/", (req, res) => {
-  res.send("Auth Route Working");
+router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Auth route working",
+  });
 });
 
 module.exports = router;
