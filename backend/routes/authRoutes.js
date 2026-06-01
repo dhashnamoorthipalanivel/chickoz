34fb1bdbc3c5e0c9b56066f45822c3d715f1aeb4
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2026-06-01 12:55:31 +0530

    Removed auth middleware temporarily

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,33 +1,26 @@
-// const express = require("express");
-// const {
-//   registerUser,
-//   loginUser,
-//   forgotPassword,
-//   resetPassword,
-//   getMe,
-//   logoutUser,
-//   sendChangePasswordOtp,
-//   verifyChangePasswordOtp,
-// } = require("../controllers/authController");
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  // forgotPassword,
+  // resetPassword,
+  // getMe,
+  // logoutUser,
+  // sendChangePasswordOtp,
+  // verifyChangePasswordOtp,
+} = require("../controllers/authController");
 
 // const { protect } = require("../middleware/authMiddleware");
-// const router = express.Router();
+const router = express.Router();
 
-// router.post("/register", registerUser);
-// router.post("/login", loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 // router.post("/forgot-password", forgotPassword);
 // router.post("/reset-password/:token", resetPassword);
 // router.get("/me", protect, getMe);
 // router.post("/logout", protect, logoutUser);
 // router.post("/send-change-password-otp", protect, sendChangePasswordOtp);
 // router.post("/verify-change-password-otp", protect, verifyChangePasswordOtp);
-
-// module.exports = router;
-
-
-const express = require("express");
-
-const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Auth Route Working");
